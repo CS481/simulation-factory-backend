c7ff9dd2cfcadcc9ff814ecbcb6bdf1001ee97ae
caydenreynolds <cayden@arclasr.net>
2020-11-03 18:51:11 -0500

    Added namespace to check_method

--- src/db/DBConnFactory.php
+++ src/db/DBConnFactory.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/MongoConn.php';
 
+// Factory method to get the classname of the IDBConn implementation
 // Why is everything in this stupid language a string?
 function DBConnFactory() : string {
   return 'SimulationFactoryBackend\MongoConn';
